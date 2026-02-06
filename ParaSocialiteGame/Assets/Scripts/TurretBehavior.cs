f08c0e0950cc2517d196c0899ca9c7b7c9fd380d
oktemeza-cyber <oktemeza@ualberta.ca>
2026-02-06 00:51:57 -0700

    Base grid made and possible side menu for towers (#1)

---
+++ ParaSocialiteGame/Assets/Scripts/TurretBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehavior : MonoBehaviour
+{
+  [SerializeField] private GameObject _highlight;
+
+
+
+    void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+}
