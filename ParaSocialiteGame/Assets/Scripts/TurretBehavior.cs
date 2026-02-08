49c14a42c72f636cf6f28d5ae7534e25274d71eb
Annie D <ading2@ualberta.ca>
2026-02-07 19:57:43 -0700

    ope's music

--- ParaSocialiteGame/Assets/Scripts/TurretBehavior.cs
+++ ParaSocialiteGame/Assets/Scripts/TurretBehavior.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class TurretBehavior : MonoBehaviour
 {
-  [SerializeField] private GameObject _highlight;
+    [SerializeField] private GameObject _highlight;
 
 
 
     void OnMouseEnter()
     {
         _highlight.SetActive(true);
+        AudioController.Instance.PlayEffect("select");
     }
 
     void OnMouseExit()
     {
         _highlight.SetActive(false);
+        AudioController.Instance.PlayEffect("place");
+
     }
 }
