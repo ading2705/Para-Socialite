45f147f171806ada13aaa0f7e799744ce89a2db6
Annie D <ading2@ualberta.ca>
2026-02-10 23:55:44 -0700

    Transitions
    Implemented transitionManager and tested transtiions between scenes

---
+++ Assets/Scripts/TowerDefenseScripts/TowerDefenseSetup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDefenseSetup : MonoBehaviour
+{
+    void Start()
+    {
+        AudioController.Instance.PlayTheme("tower");
+    }
+}
