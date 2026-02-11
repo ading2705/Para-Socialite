810dfffc6c35bf4c6621f3295dbda59514a5f844
oktemeza-cyber <oktemeza@ualberta.ca>
2026-02-10 23:41:54 -0700

    GameUpdateForTDGameflow
    Turret drag and drop added, different enemy types added, and general game flow added w/turret shooting.

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -9,23 +9,22 @@ public class GridManager : MonoBehaviour
   
   [SerializeField] private Transform _cam;
 
-  void Start()
-  {
-    GenerateGrid();
+  void Start(){
+  GenerateGrid();
+
   }
 
-  void GenerateGrid()
-  {
-    for (int x = 0; x < _width; x++)
-    {
-      for (int y = 0; y < _height; y++)
-      {
-        var randomTile = Random.Range(0, 6) == 3 ? _PathTile : _PlacementTile;
-        var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
-        spawnedTile.name = $"Tile {x} {y}";
-        spawnedTile.Init(x, y);
+  void GenerateGrid(){
+     for(int x = 0; x < _width; x++ ){
+      for(int y = 0; y < _height; y++){
+         var randomTile = Random.Range(0,6) == 3 ? _PathTile : _PlacementTile;
+         var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
+         spawnedTile.name = $"Tile {x} {y}";
+         spawnedTile.Init(x,y);
       }
     }
-    _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
-  }
+    _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height/2 -0.5f, -10);
+  }  
+
+
 }
