3bad476066c86334b286d318dee2c0bc3fa62df9
Annie D <ading2@ualberta.ca>
2026-02-11 20:23:20 -0700

    fixed for all turrets

--- Assets/Scripts/TowerDefenseScripts/HoverBehaviour.cs
+++ Assets/Scripts/TowerDefenseScripts/HoverBehaviour.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HoverBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject turretBox;
+    [SerializeField] private TMP_Text titleLabel;
+    [SerializeField] private TMP_Text descriptionLabel;
+    public string turretTitle;
+    public string turretDescription;
+
+    void OnMouseOver()
     {
-        
+        titleLabel.text = turretTitle;
+        descriptionLabel.text = turretDescription;
+        turretBox.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit()
     {
-        
+        turretBox.SetActive(false);
     }
 }
