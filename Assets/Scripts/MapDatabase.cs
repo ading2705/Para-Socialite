41763207cfda6cea6a43cfdefa1d38f193cfbcc2
SlowSAPH <lealagoss@gmail.com>
2026-02-11 17:25:50 -0700

    Updated everything from old GridManager
    xD

---
+++ Assets/Scripts/MapDatabase.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// PLEASE ADJUST THESEEEEE 
+public static class MapDatabase
+{
+    public static List<MapData> Maps = new List<MapData>()
+    {
+        // lvl 1 
+        new MapData(
+            new List<Vector2Int>() // path tiles
+            {
+                new Vector2Int(0,0),
+                new Vector2Int(1,0),
+                new Vector2Int(2,0),
+                new Vector2Int(2,1),
+                new Vector2Int(2,2),
+            }
+        ),
+
+        // lvl 2
+        new MapData(
+            new List<Vector2Int>() // path tiles
+            {
+                new Vector2Int(0,0),
+                new Vector2Int(0,1),
+                new Vector2Int(0,2),
+                new Vector2Int(1,2),
+            }
+        )
+    };
+}
