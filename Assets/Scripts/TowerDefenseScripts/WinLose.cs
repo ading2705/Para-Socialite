613f8db74e807939b0e48ed5af90827fe36fb6e8
SlowSAPH <lealagoss@gmail.com>
2026-02-11 23:49:38 -0700

    Updated WL Screen, not finished yet tho
    ;p

--- Assets/Scripts/TowerDefenseScripts/WinLose.cs
+++ Assets/Scripts/TowerDefenseScripts/WinLose.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class WinLose : MonoBehaviour
 {
+    private static WinLose _instance; //singleton
+    public static WinLose Instance // public access for singleton
+    {
+        get{return _instance;}
+    }
+    [SerializeField] private string winScene; // loaded scene on win 
+    [SerializeField] private string loseScene; // loaded scene on loss
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        //boo
+        _instance = this; // assigns "this" obj to static instnance var n lets other scripts use wl.instance
+    }
+// call WinLose.Instance.Win(); or .Lose(); to use them
+   public void Win()
+    {// tell the manager what scene we want next n then tells it to load that scene
+        TransitionManager.Instance.nextScene = winScene; 
+        TransitionManager.Instance.GoToNextScene();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Lose()
     {
-        
+        TransitionManager.Instance.nextScene = loseScene;
+        TransitionManager.Instance.GoToNextScene();
     }
+
 }
