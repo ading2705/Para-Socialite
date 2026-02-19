7df5604a3162babe5e8b9b4193a8d08c77226520
Annie D <ading2@ualberta.ca>
2026-02-18 22:08:53 -0700

    VISUAL NOVEL GAME ENGINE WITHIN UNITY WOOO
    Developed InteractableObject prefab for writer (Etha) to easily use. Changed TransitionManager to accomodate multiple potential "next scenes". Created a template Narrative Scene with required items.

--- Assets/Scripts/Managers/TransitionManager.cs
+++ Assets/Scripts/Managers/TransitionManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Assertions;
 
 public class TransitionManager : MonoBehaviour
 {
     private static TransitionManager _instance;
     public static TransitionManager Instance { get { return _instance; } }
-    public string nextScene;
+    public string[] nextScenes;
 
     private void Start()
     {
@@ -15,6 +16,25 @@ public class TransitionManager : MonoBehaviour
     }
     public void GoToNextScene()
     {
-        SceneManager.LoadScene(nextScene);
+        Assert.IsNotNull(nextScenes[0]);
+        SceneManager.LoadScene(nextScenes[0]);
+    }
+
+    public void GoToNextScene(string scene)
+    {
+        if (System.Array.IndexOf(nextScenes, scene) != -1)
+        {
+            SceneManager.LoadScene(scene);
+        }
+        else
+        {
+            Debug.Log("Loaded incorrect scene using string overload!");
+        }
+    }
+
+    public void GoToNextScene(int index)
+    {
+        Assert.IsNotNull(nextScenes[index]);
+        SceneManager.LoadScene(nextScenes[index]);
     }
 }
